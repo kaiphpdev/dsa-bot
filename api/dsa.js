e7293f5baecd51455e83c93cf57a929ae32448f7
Kuldeep Kumar <kaiphpdev@gmail.com>
2026-04-10 18:29:59 +0530

    fix cron config

--- api/dsa.js
+++ api/dsa.js
@@ -7,7 +7,7 @@ import nodemailer from "nodemailer";
 // ======================
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 // const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 
 // ======================
 // 🗄️ SUPABASE SETUP
